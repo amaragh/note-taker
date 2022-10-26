5ac35d9c34295885b0b3c81d0910c9c7ba2244d9
amaragh <mercer.alecia@gmail.com>
2022-10-25 22:26:27 -0500

    add post notes

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,14 +1,27 @@
+const fs = require('fs');
 const router = require('express').Router();
 const path = require('path');
 const notes = require('../../db/db.json');
-
+const crypto = require('crypto');
 
 router.get('/notes', (req, res) => {
-   res.json(notes);
+    res.json(notes);
 });
 
 router.post('/notes', (req, res) => {
+    let newNote = {
+        title: req.body.title,
+        text: req.body.text,
+        id: crypto.randomUUID()
+    }
+    notes.push(newNote)
 
+    console.log(notes);
+    
+    fs.writeFileSync(
+        path.join(__dirname, "../../db/db.json"),
+        JSON.stringify({ notes }, null, 2)
+    );
 });
 
 
